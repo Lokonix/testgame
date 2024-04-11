89b1f4c9f28fe9184809272b1505b9522bf1f41a
Natan Wiselka <natanwiselka20@gmail.com>
2024-04-11 17:37:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 400
 WINDOW = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Niebieskie tło w Pygame")
-
+abc = pygame.image.load("img/map_test.png")
 # Kolory
 BLUE = (0, 0, 255)  # Składniki RGB: czerwony, zielony, niebieski
 
@@ -23,7 +23,7 @@ async def main():
 
         # Wypełnienie tła kolorem niebieskim
         WINDOW.fill(BLUE)
-
+        WINDOW.blit(abc, (0, 0))
         # Aktualizacja ekranu
         pygame.display.update()
         await asyncio.sleep(0)
